17f5931dd73ec0fabddadd5e26d695b56595e8eb
shrestha-das <shresthadas.0.infinity@gmail.com>
2025-12-01 19:51:59 +0530

    Number Guessing Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,12 @@
 use std::io; // for handling user input
 use std::cmp::Ordering; // for comparing values
 use rand::Rng; // for generating random numbers
+use colored::*; // for colored terminal output
 
 fn main() {
     println!("Guess the number!");
     
-    let secret_number = rand::thread_rng().gen_range(1..=101);  // generates a random number between 1 and 100 inclusive
+    let secret_number = rand::thread_rng().gen_range(1..=100);  // generates a random number between 1 and 100 
  
     loop {
     println!("Please input your guess.");
@@ -16,15 +17,25 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    // shadowing the previous guess variable with a new one of type u32
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please enter a valid number!");
+            continue;
+        }
+    };
+
 
     println!("You guessed: {guess}");
 
     match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
+        Ordering::Less => println!("{}", "Too small!".red()),
+        Ordering::Greater => println!("{}", "Too big!".red()),
         Ordering::Equal => { 
-        println!("You win! The number was {secret_number}.");
+        println!("{}", "You win!".green());
+        println!("The secret number was: {secret_number}");
         break;
         },
     }
---
+++ guessing_game/target/debug/deps/guessing_game-7bbdf5e403c907cd.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/shres/Desktop/RUST/Lets-get-Rusty/guessing_game/target/debug/deps/guessing_game-7bbdf5e403c907cd.d: src/main.rs
+
+/mnt/c/Users/shres/Desktop/RUST/Lets-get-Rusty/guessing_game/target/debug/deps/guessing_game-7bbdf5e403c907cd: src/main.rs
+
+src/main.rs:
